2f8bdba6ff2c8ea00a70ba749d77f05b235e6591
ft80 <slexodusfl@gmail.com>
2023-02-28 11:17:06 +0330

    add font

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,166 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+      <v-row align="center" justify="center" >
+          <v-col cols="12" sm="10">
+            <v-card class="elevation-6 mt-10"  >
+             <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="6">
+                      <v-card-text class="mt-12">
+                        <v-row align="center" justify="center">
+                          <v-col cols="12" sm="8">
+                           
+                          <v-text-field
+                            label="ایمیل"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                           class="mt-16"
+                          />
+                          <v-text-field
+                            label="رمز عبور"
+                            outlined
+                            dense
+                            color="blue"
+                          autocomplete="false"
+                           type="password"
+                          
+                          />
+                            <v-row>
+                              <v-col cols="12" sm="7">
+                                
+                              </v-col>
+                              <v-col cols="12" sm="5">
+                                <span class="caption blue--text">فراموشی رمزعبور</span>
+                              </v-col>
+                            </v-row>
+                          <v-btn color="blue" dark block tile>ورود</v-btn>
+                          </v-col>
+                        </v-row>  
+                      </v-card-text>
+                    </v-col>
+                    <v-col cols="12" md="6" class="blue darken-4 rounded-bl-xl" >
+                    <div style="  text-align: center; padding: 180px 0;">
+                      <v-card-text class="white--text" >
+                        <div class="justify-center">
+                        <v-img contain max-height="200" src="https://mhrajati.ir/wp-content/uploads/2022/06/Shahrood2-1.png"></v-img>
+                        </div>
+                        <br/>
+                        <br/>
+                        <h3 class="text-center ">تا کنون عضو نشده‌اید؟</h3>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn tile outlined dark @click="step++">عضویت</v-btn>
+                      </div>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row >
+                    <v-col cols="12" md="6" class="blue darken-4 rounded-br-xl">
+                     <div style="  text-align: center; padding: 180px 0;">
+                      <v-card-text class="white--text" >
+                        <div class="justify-center">
+                        <v-img contain max-height="200" src="https://mhrajati.ir/wp-content/uploads/2022/06/Shahrood2-1.png"></v-img>
+                        </div>
+                        <br/>
+                        <br/>
+                        <h2 class="text-center ">قبلا ثبت نام کرده‌اید؟</h2>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn tile outlined dark @click="step--">ورود</v-btn>
+                      </div>
+                      </div>
+                    </v-col>
+
+                    <v-col cols="12" md="6">
+                      <v-card-text class="mt-12">
+                        <v-row align="center" justify="center">
+                          <v-col cols="12" sm="8">
+                           <v-row>
+                           <v-col cols="12" sm="6">
+                            <v-text-field
+                            label="نام خانوادگی"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                           class="mt-4"
+                          />
+                           </v-col>
+                           <v-col cols="12" sm="6">
+                            <v-text-field
+                            label="نام"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                           class="mt-4"
+                          />
+                           </v-col>
+                           </v-row>
+                          
+                          <v-text-field
+                            label="ایمیل"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                          />
+                          <v-text-field
+                            label="نام کاربری"
+                            outlined
+                            dense
+                            color="blue"
+                            autocomplete="false"
+                           class="mt-4"
+                          />
+                          <v-text-field
+                            label="رمز عبور"
+                            outlined
+                            dense
+                            color="blue"
+                          autocomplete="false"
+                           type="password"
+                          
+                          />
+                          
+                          <v-btn color="blue" dark block tile>ثبت نام</v-btn>
+                          </v-col>
+                        </v-row>  
+                      </v-card-text>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+      </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+   data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
+  } 
+
+    
   }
-}
 </script>
+<style scoped>
+.v-application .rounded-bl-xl {
+    border-bottom-left-radius: 300px !important;
+}
+.v-application .rounded-br-xl {
+    border-bottom-right-radius: 300px !important;
+}
+</style>
